fd70e5a1e6dc55c756ac408dcd16c5959ebd3d97
g-depaola <gdepaola9234@gmail.com>
2024-03-02 17:49:59 -0500

    Modified EULA script

--- update_eula.sh
+++ update_eula.sh
@@ -3,12 +3,12 @@
 ### Change EULA to true
 
 # Define the filename
-filename="EULA.txt"
+filename="eula.txt"
 
 # Check if the file exists
 if [ -f "$filename" ]; then
     # Use sed to find and replace the value
-    sed -i 's/EULA=false/EULA=true/g' "$filename"
+    sed -i 's/eula=false/eula=true/g' "$filename"
     echo "EULA value changed to true."
 else
     echo "File $filename does not exist."
