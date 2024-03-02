4b87643906b4fc765d7d0b10c855f44b1ba260b9
g-depaola <gdepaola9234@gmail.com>
2024-03-02 17:47:24 -0500

    Updated syntax

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$script_dir = $(dirname $0)
+script_dir=$(dirname $0)
 
 # Update and upgrade repositories.
 apt update && apt upgrade -y
@@ -12,16 +12,16 @@ mkdir minecraft_server
 cd minecraft_server
 
 # Make scripts executable
-sudo chmod u+x start_server.sh && sudo chmod u+x update_eula.sh
+sudo chmod u+x ${script_dir}/start_server.sh && sudo chmod u+x ${script_dir}/update_eula.sh
 
 # Download server .jar file
 wget https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar
 
 # Execute start_server.sh
-bash .$script_dir/start_server.sh
+bash .${script_dir}/start_server.sh
 
 # Update EULA
-bash .$script_dir/update_eula.sh
+bash .${script_dir}/update_eula.sh
 
 # Execute start_server.sh
-bash .$script_dir/start_server.sh
+bash .${script_dir}/start_server.sh
