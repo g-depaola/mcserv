fedc0b444758d7b6da2d4c7a1244285753c8e783
g-depaola <gdepaola9234@gmail.com>
2024-03-02 19:26:58 -0500

    Updated line 14 to fix execution.

--- initial_setup.sh
+++ initial_setup.sh
@@ -9,10 +9,12 @@ apt install openjdk-17-jre-headless -y
 
 # Create server directory
 mkdir minecraft_server
-cd minecraft_server
 
 # Make scripts executable
-sudo chmod u+x "$script_dir/start_server.sh" && sudo chmod u+x "$script_dir/update_eula.sh" && sudo chmod u+x "$script_dir/update_server_properties.sh"
+chmod u+x start_server.sh && chmod u+x update_eula.sh && chmod u+x update_server_properties.sh
+
+# Go to server directory.
+cd minecraft_server
 
 # Download server .jar file
 wget https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar
